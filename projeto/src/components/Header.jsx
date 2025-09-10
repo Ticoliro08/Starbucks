b1fac154cf2c6a7640262a2560a7699192603b75
Ticoliro08 <ggsantana18@gmail.com>
2025-09-09 22:44:16 -0300

    att2

--- projeto/src/components/Header.jsx
+++ projeto/src/components/Header.jsx
@@ -3,19 +3,19 @@ import { ShoppingCart, Sun } from 'lucide-react';
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-6 bg-white shadow-md">
+    <header className="logo">
       <div className="flex items-center gap-2">
         <div className="w-8 h-8 bg-green-800 rounded-full flex items-center justify-center text-white font-bold">★</div>
         <span className="font-bold text-green-900">Starbucks</span>
       </div>
 
-      <nav className="flex gap-6">
+      <nav className="active">
         <Link to="/" className="hover:text-green-800">Home</Link>
         <Link to="/menu" className="hover:text-green-800">Menu</Link>
         <Link to="/contato" className="hover:text-green-800">Contato</Link>
       </nav>
 
-      <div className="flex gap-4">
+      <div className="header-icons">
         <Sun size={20} className="cursor-pointer"/>
         <ShoppingCart size={20} className="cursor-pointer"/>
       </div>
--- projeto/src/pages/Home.jsx
+++ projeto/src/pages/Home.jsx
@@ -1,19 +1,15 @@
 import Button from '../components/Button';
-import heroImage from './assets/starbucks-frappuccino.jpg'; // coloque a imagem na pasta assets
-
+import heroImage from '../assets/starbucks-hero.jpg'; // coloque a imagem na pasta assets
+import '../index.css'
 export default function Home() {
   return (
-    <div className="relative">
-      <div className="absolute inset-0 bg-green-900/70"></div>
-      <img src={heroImage} alt="Starbucks" className="w-full h-screen object-cover"/>
-      
-      <div className="absolute inset-0 flex flex-col justify-center items-center text-center px-4">
-        <h1 className="text-white text-5xl font-bold mb-4">Bem-vindo ao <span className="text-yellow-500">Starbucks</span></h1>
-        <p className="text-white text-lg mb-6">Desperte seus sentidos com nossa seleção premium de cafés e bebidas artesanais</p>
-        <div className="flex gap-4">
-          <Button>Explorar Menu</Button>
-        </div>
-      </div>
-    </div>
+  <section className="hero">
+  <div className="hero-content">
+    <h1>Bem-vindo ao <span>Starbucks</span></h1>
+    <p>Desperte seus sentidos com nossa seleção premium de cafés e bebidas artesanais</p>
+    <button className="btn btn-primary">Explorar Menu</button>
+    <button className="btn btn-secondary">Saiba Mais</button>
+  </div>
+</section>
   )
 }
